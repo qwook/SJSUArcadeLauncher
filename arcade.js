901f24ee647f45fc807e58bfa54e768bd95e1ee8
Henry <henryquoctran@gmail.com>
2014-04-19 15:59:51 -0700

    Fixed carousel & added insert
    * Made it so games on the left and right hide themselves
    * Fixed left and right carousel
    * Added insertGame code

--- arcade.js
+++ arcade.js
@@ -1,38 +1,48 @@
 // Arcade Frontend
 
-// a - 65, d - 68, w - 87, s - 83
-// up - 38, down - 40, left - 37, right - 39
+// hide other 'gameshows'
+function hideOthers() {
+    $('.gameshow.left').addClass("hidden")
+    .last().removeClass("hidden")
 
+    $('.gameshow.right').addClass("hidden")
+    .first().removeClass("hidden")
+}
+
+// slide gameshows to the left
 function moveLeft() {
-    var left = $('.gameshow.left')
-    left = left[left.length-1]
+    var left = $('.gameshow.left').last()
     // if there is any games on the left
-    if (left) {
+    if (left[0]) {
         // move the original featured game over
         $('.gameshow.featured')
             .removeClass("featured")
             .addClass("right")
 
-        $(left)
+        left
             .removeClass("left")
             .addClass("featured")
     }
+
+    hideOthers();
 }
 
+// slide gameshows to right
 function moveRight() {
-    var right = $('.gameshow.right')
-    right = right[right.length-1]
+    var right = $('.gameshow.right').first()
     // if there is any games on the left
-    if (right) {
+    if (right[0]) {
         // move the original featured game over
         $('.gameshow.featured')
             .removeClass("featured")
             .addClass("left")
 
-        $(right)
+        right
             .removeClass("right")
             .addClass("featured")
     }
+
+    hideOthers();
 }
 
 var launchGameCb;
@@ -45,8 +55,44 @@ function reset() {
     $("#games").removeClass("loading")
 }
 
-function insertGame() {
+function insertGame(data) {
+    // todo: switch to templating system and use handlebar
+    // but it's way too simple to have that...
+    var gameshow = $(
+        '<div class="gameshow">'
+            + '<div class="screenshot">'
+            + '<div class="caption">'
+                + '<div class="caption-left">'
+                    + '<h1></h1>'
+                    + '<p class="authors"></p>'
+                + '</div>'
+                + '<div class="caption-right">'
+                    + '<p class="description"></p>'
+                + '</div>'
+            + '</div>'
+        + '</div>'
+        )
 
+    if ($(".gameshow").length > 0)
+        gameshow.addClass("right");
+    else
+        gameshow.addClass("featured");
+
+    $(".screenshot", gameshow).css({
+        "background-image": "url(" + data.screenshot + ")"
+    });
+
+    $(".description", gameshow).text(data.description || "");
+
+    $("h1", gameshow).text(data.name || "!! No name added !!");
+
+    $(".authors", gameshow).text((data.authors || []).join(", "));
+
+    console.log((data.authors || []).join(", "))
+
+    $("#games").append(gameshow);
+
+    hideOthers();
 }
 
 function setLaunchGameCallback(cb) {
