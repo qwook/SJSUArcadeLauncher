6c179c092fb1bdaf152dc3a3480c28f9ac5ed0ca
Henry <henryquoctran@gmail.com>
2014-04-19 15:59:18 -0700

    Added comments & test insertGame

--- backend.js
+++ backend.js
@@ -1,3 +1,4 @@
+
 // using the require() function will break in browsers
 // this file will exit with an error if it runs in the browser
 var fs = require("fs"),
@@ -6,8 +7,12 @@ var fs = require("fs"),
 
 $("#games").html(""); // clear the games for backend
 
+// Try to find the config.json file!
 var CONFIG_PATH = path.join(process.execPath, "config.json");
 var CONFIG_PATH_DEV = path.join(process.cwd(), "config.json");
+
+// The working directory should be the directory
+// the config.json file is in.
 var WORKING_DIRECTORY;
 
 var configStr;
@@ -22,3 +27,20 @@ if (fs.existsSync(CONFIG_PATH)) {
 }
 
 var config = JSON.parse(stripJSON(configStr));
+
+insertGame({});
+insertGame({});
+insertGame({});
+insertGame({
+    "screenshot": "http://globalgamejam.org/sites/default/files/styles/game_content__normal/public/games/screenshots/screenshot_2014-01-26_16.27.03.png?itok=h6D-8YiZ"
+});
+
+insertGame(
+    {
+        "name": "Test Game",
+        "authors": ["Test Person", "Test Person2", "John Smith", "Jane Doe"],
+        "description": "This is a test description blah blah hi there!",
+        "screenshot": "http://placehold.it/640x480"
+    }
+);
+
